1313fb08876fbad1ddf114dab91f425e9188544b
Teddy Bermes <bermessa@gmail.com>
2018-01-18 02:33:47 +0000

    mainpanel

--- client/src/pages/landingpage/landingpage.js
+++ client/src/pages/landingpage/landingpage.js
@@ -2,9 +2,10 @@
 import React, {Component} from "react";
 import NavBar from "../../components/navbar"; 
 import Container from "../../components/container";
-import pTextBox from "../../components/paragraphdiv/ptextbox";
-import pTitle from "../../components/paragraphdiv/ptitle";
-import ContactForm from "../../components/forms/contactform";
+import MainPanel from "../../components/mainpanel"
+// import pTextBox from "../../components/paragraphdiv/ptextbox";
+// import pTitle from "../../components/paragraphdiv/ptitle";
+// import ContactForm from "../../components/forms/contactform";
 import CakeForm from "../../components/forms/cakeform";
 // import Footer from "./components/footer";
 // import { Link } from "react-router-dom";
@@ -16,7 +17,9 @@ render () {
     return (
         <Container>
             <NavBar />
-            <CakeForm />
+                <MainPanel>
+                    <CakeForm />
+                </MainPanel>
         </Container>
 )}
 

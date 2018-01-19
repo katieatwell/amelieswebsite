14cf28505cd8cb9902d56a31dd47951ff881cff5
Teddy Bermes <bermessa@gmail.com>
2018-01-19 20:59:22 +0000

    cateringevents

--- client/src/components/container/container.js
+++ client/src/components/container/container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 export default class Wrapper extends React.Component {
@@ -7,7 +8,11 @@ export default class Wrapper extends React.Component {
   render() {
     return (
       <Container>
-        {this.props.children}
+        <Row>
+          <Col xs="12" md="12">
+            {this.props.children}
+          </Col>
+        </Row>
       </Container>
     );
   }

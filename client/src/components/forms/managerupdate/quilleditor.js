e9905268456747abd06b3d17f5429b4783cad8b3
Stephen Chapman <schapm16@gmail.com>
2018-01-20 17:49:02 +0000

    Initialize QuillJS React Component

---
+++ client/src/components/forms/managerupdate/quilleditor.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactQuill, { Quill } from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+// This changes Quill from using class ="ql-align-<center or right>" to define
+// text justification to using style = "text-align: <center or right>"
+const AlignStyle = Quill.import('attributors/style/align');
+Quill.register(AlignStyle, true);
+
+// Future: There is not an inline style option for indent.  
+// In order to accomodate indent, we need a custom css that styles indents based on the element class.
+// const IndentStyle = Quill.import('attributors/style/indent');
+// Quill.register(IndentStyle, true);
+
+class QuillEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { html: ''}; //Future: html will be pre-filled based on user selection
+    this.modules = {
+      toolbar: [  // Formats included in the Quill editor toolbar
+        ['bold', 'italic', 'underline'],
+        [{ 'align': '' }, { 'align': 'center' }, { 'align': 'right' }],
+        [{ 'header': [2, 3, 4, false] }],
+        [{ 'indent': '+1' }, { 'indent': '-1' }],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }]
+      ]
+    };
+    this.formats = [  // Formats that the  Quill editor will allow
+      'bold',
+      'italic',
+      'underline',
+      'align',
+      'header',
+      'indent',
+      'list',
+    ];
+  }
+
+  handleChange = (value) => {
+    this.setState({ html: value });
+  }
+
+  render() {
+    return <ReactQuill value={this.state.html} onChange={this.handleChange} modules={this.modules} formats={this.formats}/>;
+  }
+}
+
+export default QuillEditor;

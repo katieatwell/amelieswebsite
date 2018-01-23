f69cdf836775a9af00c8f4962a603380a7ef7d18
Teddy Bermes <bermessa@gmail.com>
2018-01-23 02:13:42 +0000

    nav bar and UI work in progress

--- client/src/pages/catering/special.js
+++ client/src/pages/catering/special.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import NavBar from "../../components/navbar"; 
+import React, { Component } from "react";
+import NavBar from "../../components/navbar";
 import Container from "../../components/container";
 import MainPanel from "../../components/mainpanel"
 import PanelTitle from "../../components/paragraphdiv/ptitle";
@@ -12,47 +12,49 @@ import "./style.css";
 
 class LandingPage extends Component {
 
-render () {
-    return (
-        <Container>
-            <NavBar />
-                <MainPanel>
-                    <PanelTitle>
-                        Wedding Events
-                    </PanelTitle>
-                    <PanelBody>
-                        <Row>
-                        <Col xs="12" sm="12" md="12" className="specialEventsDiv">
-                            <ReactPlayer url="https://player.vimeo.com/video/102756893?color=8e9191&title=0&portrait=0" width="auto"/>
-                            <br /><br />
-                            
-                            With fun, colorful dessert bars and elegantly hand prepared Wedding Cakes, Amélie's can make your Wedding Day both memorable and delicious. We also provide all day packages to keep your Wedding Party relaxed and energized as you prepare for the main event.  Let us be your something blue!
-                            <br /><br />
-                            
-                            Contact Summer Williams for a wedding cake consultation at cakes@ameliesfrenchbakery.com
-                            <br /><br />
-                            
-                            Cake Tastings for $15!
-                            Sample 3 cake varieties, 3 fillings, and 3 buttercream flavors while meeting with our Specialty Cake Director.
-                            (Tasting cost plus tax will be deducted from the final booking cost.)
-                            <br /><br />
-                            
-                            Tiered Cakes at $5.25 per serving
-                            <br /><br />
-                            
-                            Delivery Available
-                            $50 Delivery Fee within 40 miles round trip of Noda Bakery. Each additional mile will be charged $1.00 per mile.
- 
-                        
-                        </Col>
-                        </Row>
-
-                    </PanelBody>
-                </MainPanel>
-            <Footer />
-        </Container>
-)}
-
+    render() {
+        return (
+            <div>
+                <NavBar />
+                    <Container>
+                        <MainPanel>
+                            <PanelTitle>
+                                Wedding Events
+                            </PanelTitle>
+                            <PanelBody>
+                                <Row>
+                                <Col xs="12" sm="12" md="12" className="specialEventsDiv">
+                                    <ReactPlayer url="https://player.vimeo.com/video/102756893?color=8e9191&title=0&portrait=0" width="auto"/>
+                                    <br /><br />
+                                    
+                                    With fun, colorful dessert bars and elegantly hand prepared Wedding Cakes, Amélie's can make your Wedding Day both memorable and delicious. We also provide all day packages to keep your Wedding Party relaxed and energized as you prepare for the main event.  Let us be your something blue!
+                                    <br /><br />
+                                    
+                                    Contact Summer Williams for a wedding cake consultation at cakes@ameliesfrenchbakery.com
+                                    <br /><br />
+                                    
+                                    Cake Tastings for $15!
+                                    Sample 3 cake varieties, 3 fillings, and 3 buttercream flavors while meeting with our Specialty Cake Director.
+                                    (Tasting cost plus tax will be deducted from the final booking cost.)
+                                    <br /><br />
+                                    
+                                    Tiered Cakes at $5.25 per serving
+                                    <br /><br />
+                                    
+                                    Delivery Available
+                                    $50 Delivery Fee within 40 miles round trip of Noda Bakery. Each additional mile will be charged $1.00 per mile.
+         
+                                
+                                </Col>
+                                </Row>
+        
+                            </PanelBody>
+                        </MainPanel>
+                    </Container>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default LandingPage;

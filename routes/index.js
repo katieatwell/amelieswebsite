8f84cf2757e6e155c850fc374d158be3524f6677
Pat hayes <pat2382@yahoo.com>
2018-01-23 17:22:43 +0000

    added passport authenticate method to apply to any route starting with 'api/manager'. any attempt to get into this route will need a header with 'Authorization: jwt <insert token here>'

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const path = require('path');
 router.use(express.static(path.join(__dirname + '/client/build')));
 router.use('/api', require('./apiRoutes') );
 
-router.get('*', path.join(__dirname + '/client/build/index.html'));
+// router.get('*', path.join(__dirname + '/client/build/index.html'), function(req,res){
+//   res.json("Success! You can only see this with a token!")
+// });
 
 module.exports = router;

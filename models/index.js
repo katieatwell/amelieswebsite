205611fb4f9737480e134436e6ea9d9974d76f98
Stephen Chapman <schapm16@gmail.com>
2018-01-24 18:36:33 +0000

    Modify Cake Schema and Model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   ManagerCredential: require('./managercredential.js'),
   CCMenu: require('./cafe-cateringmenu.js'),
-  CakeMenu: require('./cakemenu.js'),
+  CakeData: require('./cakedata.js'),
 };

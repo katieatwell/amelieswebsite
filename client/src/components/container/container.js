e8896837673f832453e60496d1c745870989b06b
Teddy Bermes <bermessa@gmail.com>
2018-01-24 01:39:35 +0000

    footer and media queries

--- client/src/components/container/container.js
+++ client/src/components/container/container.js
@@ -8,17 +8,13 @@ export default class Wrapper extends React.Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Col xs="12" md="12">
             {this.props.children}
-          </Col>
-        </Row>
       </Container>
     );
   }
 }
-    
+
 Container.propTypes = {
-  fluid:  PropTypes.bool
+  fluid: PropTypes.bool
   // applies .container-fluid class
 };

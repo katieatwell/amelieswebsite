69f3d3bece9ea993db22dff8ff4490cf8337f432
Stephen Chapman <schapm16@gmail.com>
2018-01-24 03:53:52 +0000

    Corrected relative paths for controllers in apiRoutes/manager.js and apiRoutes/menu.js

--- routes/apiRoutes/menu.js
+++ routes/apiRoutes/menu.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const CCMenu = require('../../controller/ccmenu');
-const CakeMenu = require('../../controller/cakemenu');
+const CCMenu = require('../../controllers/ccmenu');
+const CakeMenu = require('../../controllers/cakemenu');
 
 router.get('/cafe-menu', CCMenu.getCafeALL);
 router.get('/catering-menu', CCMenu.getCateringALL);

8db4ab660922404588040edfb8233f030c21689d
Pat hayes <pat2382@yahoo.com>
2018-01-27 20:27:20 +0000

    commented out seeds and put note for removal after used once on heroku

--- controllers/seedDB.js
+++ controllers/seedDB.js
@@ -1,32 +1,27 @@
 var db = require("../models");
-
-
 const bcrypt=require("bcrypt");
 
 //remember, passwords should never be stored plain text like here but use bcrypt 12 or greater before storing to db
-console.log("running double check");
 //set up original user and password. 
 //store with bcrypt - seed with encryption
 
+//this code will run automatically if not commented out. Only want to run this Once and then remove from heroku.
+/*bcrypt.hash('tested',12,function(err, hash){
+if (err)console.error(err);
+db.AuthUser
+  .create({ name: "test3", password:hash })
+  .then(function(dbAuthUser) {
+    // If saved successfully, print 
+    console.log(dbAuthUser);
+  })
+  .catch(function(err) {
+    // If an error occurs, print it to the console
+    console.log(err.message);
+  });
+})*/
 
-    console.log("hit seed function")
-    bcrypt.hash('tested',12,function(err, hash){
-    if (err)console.error(err);
-    console.log('bcrypt running, hash: '+ hash);
-    db.AuthUser
-      .create({ name: "test3", password:hash })
-      .then(function(dbAuthUser) {
-        // If saved successfully, print 
-        console.log(dbAuthUser);
-      })
-      .catch(function(err) {
-        // If an error occurs, print it to the console
-        console.log(err.message);
-      });
-    })
- 
-    db.AuthUser.find(function(err,user){
-      if (err) return console.error(err);
-      console.log(user);
-    })
+/*db.AuthUser.find(function(err,user){
+  if (err) return console.error(err);
+  console.log(user);
+})*/
 

d3b8e3f8edd8e142b602e970f23e08f2be027017
Stephen Chapman <schapm16@gmail.com>
2018-01-27 20:48:52 +0000

    Change object build scheme for composed cakes prior to sending to client.  Change req.body.id to req.body._id in manager controller for consistency. Send status code 500 to client  in manager controller if database action produces an error.

--- controllers/cakemenu.js
+++ controllers/cakemenu.js
@@ -12,7 +12,7 @@ let CakeMenu = {
   getComposedCakesALL: function(req, res) {
     CakeMenu.findDescriptors('ComposedCakes').then(function(descriptors) {
       CakeMenu.queryByDescriptors(descriptors).then(function(data) {
-        res.json(CakeMenu.buildObject(data));    
+        res.json(CakeMenu.buildComposedCakesObject(data));    
       });
     });
   },
@@ -32,7 +32,7 @@ let CakeMenu = {
   queryByDescriptors: function(descriptors) {
     let qPromises = [];
     descriptors.forEach(function(descriptor) {
-      qPromises.push(db.CakeData.find({ 'descriptor': descriptor }));
+      qPromises.push(db.CakeData.find({ 'descriptor': descriptor }).sort('detail'));
     });
     return Promise.all(qPromises);
   },
@@ -42,6 +42,19 @@ let CakeMenu = {
     data.forEach(function(each) {
       if (each.length > 0) {
         results[each[0].descriptor] = each;
+      }
+    });
+    return results;
+  },
+  
+  buildComposedCakesObject: function(data) {
+    let results =[];
+    data.forEach(function(each) {
+      let cake = {};
+      if (each.length > 0) {
+        cake.name = each[0].descriptor;
+        cake.data = each;
+        results.push(cake);
       }
     });
     return results;

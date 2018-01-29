3f41c17ebe92f6299f11e280fa4f492322fe57d2
Pat hayes <pat2382@yahoo.com>
2018-01-29 19:10:33 +0000

    email is working when you give the api key as env variable at contact form

--- client/src/components/forms/contactform/contactform.js
+++ client/src/components/forms/contactform/contactform.js
@@ -1,37 +1,55 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-
-const sendmail=require("./../../../../../email_setup/mailer.js");
-
-const handleSubmit = (event) => {
-  event.preventDefault;
-  console.log("submit button hit")
-  
-}
+import API from './../../../utils/API';
 
 export default class ContactForm extends React.Component {
+  
+  state= {
+    name: "",
+    phone: "",
+    email:"",
+    text:"",
+    subject:"#Placeholder#testing",
+    select:"Become a VIP member!"
+  };
+  
+  handleSubmit = (event) => {
+    event.preventDefault;
+    console.log("submit button hit")
+    console.log(this.state);
+    API.sendEmail(this.state.email, this.state.select,"Hello there, this is "+this.state.name+" and my number is:"+this.state.phone+" ... I have a message: " +this.state.text)
+}
+  
+  
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  
   render() {
     return (
       <Form>
       
         <FormGroup>
           <Label for="name">Name</Label>
-          <Input type="text" name= "name" id="formName" />
+          <Input type="text" name= "name" id="formName" onChange={this.handleInputChange}/>
         </FormGroup>
         
         <FormGroup>
           <Label for="name">Phone Number</Label>
-          <Input type="text" name="phone" id="formNumber" />
+          <Input type="text" name="phone" id="formNumber" onChange={this.handleInputChange}/>
         </FormGroup>
         
         <FormGroup>
           <Label for="email">Email</Label>
-          <Input type="email" name="email" id="formEmail" />
+          <Input type="email" name="email" id="formEmail" onChange={this.handleInputChange}/>
         </FormGroup>
         
         <FormGroup>
           <Label for="directory">Directory</Label>
-          <Input type="select" name="select" id="formDirectory">
+          <Input type="select" name="select" id="formDirectory" onChange={this.handleInputChange}>
             <option>Become a VIP member!</option>
             <option>Management</option>
             <option>Catering & Events</option>
@@ -43,10 +61,10 @@ export default class ContactForm extends React.Component {
         
         <FormGroup>
           <Label for="message">Message</Label>
-          <Input type="textarea" name="text" id="formMessage" />
+          <Input type="textarea" name="text" id="formMessage" onChange={this.handleInputChange} />
         </FormGroup>
 
-        <Button onClick={handleSubmit}>Submit</Button>
+        <Button onClick={this.handleSubmit}>Submit</Button>
         <br/>
         
       </Form>
